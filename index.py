91a22b4042e9a0358e72f9cbfbfd07c33c189c96
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-07-09 15:04:39 +0100

    add data

--- index.py
+++ index.py
@@ -1,11 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET'])
-def home():
-    return "hello world"
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from wsgi import app
