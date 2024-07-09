6980de64d9e16b1a96586c42e012898ad9ba41a2
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-07-09 13:05:32 +0100

    add data

--- api/index.py
+++ api/index.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+app.config['MYSQL_HOST'] = "mysql-1fb82b3b-boukhar-d756.e.aivencloud.com"
+app.config['MYSQL_USER'] = "avnadmin"
+app.config['MYSQL_PASSWORD'] = "AVNS_wWoRjEZRmFF5NgjGCcY"
+app.config['MYSQL_DB'] = "defaultdb"
 
-@app.route('/about')
-def about():
-    return 'About'
+mysql = MySQL(app)
+
+@app.route('/contacts', methods=['GET'])
+def get_contacts():
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM contacts')
+        data = cur.fetchall()  
+        cur.close()
+        return jsonify({'contacts': data}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
