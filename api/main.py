878559e87615c3c63caddf9ba255c9d9a5b85f38
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-07-09 15:21:34 +0100

    add main.py

---
+++ api/main.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({'message': 'Hello, World!'})
+
+@app.route('/data')
+def get_data():
+    connection = mysql.connector.connect(
+        host='mysql-1fb82b3b-boukhar-d756.e.aivencloud.com',
+        user='avnadmin',
+        password='AVNS_wWoRjEZRmFF5NgjGCcY',
+        database='defaultdb'
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM contacts")
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run()
