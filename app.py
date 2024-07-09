a8d59900693dadac0c2c70e3d9c42aa8da32d7ff
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-07-09 15:14:08 +0100

    add data

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://avnadmin:AVNS_wWoRjEZRmFF5NgjGCcY@mysql-1fb82b3b-boukhar-d756.e.aivencloud.com:20744/defaultdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-# class Contact(db.Model):
-#     __tablename__ = 'contacts'
-#     id = db.Column(db.Integer, primary_key=True)
-#     full_name = db.Column(db.String(255))
-#     email = db.Column(db.String(255))
-#     subject = db.Column(db.String(255))
-#     message = db.Column(db.String(255))
+class Contact(db.Model):
+    __tablename__ = 'contacts'
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    subject = db.Column(db.String(255))
+    message = db.Column(db.String(255))
 
 
 
@@ -24,13 +24,13 @@ def start():
     return "The MBSA Server is Running"
 
 
-# @app.route('/contacts', methods=['GET'])
-# def get_contacts():
-#     try:
-#         contacts = Contact.query.all()
-#         return jsonify({'contacts': [{'id': contact.id, 'full_name': contact.full_name, 'email': contact.email, 'subject': contact.subject, 'message': contact.message} for contact in contacts]})
-#     except Exception as e:
-#         return jsonify({'error': 'Database query error', 'details': str(e)}), 500
+@app.route('/contacts', methods=['GET'])
+def get_contacts():
+    try:
+        contacts = Contact.query.all()
+        return jsonify({'contacts': [{'id': contact.id, 'full_name': contact.full_name, 'email': contact.email, 'subject': contact.subject, 'message': contact.message} for contact in contacts]})
+    except Exception as e:
+        return jsonify({'error': 'Database query error', 'details': str(e)}), 500
 
 
 @app.route("/mbsa")
