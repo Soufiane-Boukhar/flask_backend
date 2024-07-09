87e7b683ed76472603ebed349ad5041886323825
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-07-09 15:27:36 +0100

    add main.py

--- api/main.py
+++ api/main.py
@@ -1,27 +1,39 @@
 import os
+import logging
 from flask import Flask, jsonify
 import mysql.connector
 
 app = Flask(__name__)
 
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+
 @app.route('/')
 def index():
+    app.logger.debug('Index route called')
     return jsonify({'message': 'Hello, World!'})
 
-@app.route('/data',methods=['GET'])
+@app.route('/data', methods=['GET'])
 def get_data():
-    connection = mysql.connector.connect(
-        host='mysql-1fb82b3b-boukhar-d756.e.aivencloud.com',
-        user='avnadmin',
-        password='AVNS_wWoRjEZRmFF5NgjGCcY',
-        database='defaultdb'
-    )
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM contacts")
-    result = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return jsonify(result)
+    app.logger.debug('Data route called')
+    try:
+        connection = mysql.connector.connect(
+            host='mysql-1fb82b3b-boukhar-d756.e.aivencloud.com',
+            user='avnadmin',
+            password='AVNS_wWoRjEZRmFF5NgjGCcY',
+            database='defaultdb'
+        )
+        cursor = connection.cursor()
+        app.logger.debug('Database connection established')
+        cursor.execute("SELECT * FROM contacts LIMIT 10") 
+        result = cursor.fetchall()
+        app.logger.debug('Query executed successfully')
+        cursor.close()
+        connection.close()
+        return jsonify(result)
+    except mysql.connector.Error as err:
+        app.logger.error(f'Database error: {err}')
+        return jsonify({'error': str(err)}), 500
 
 if __name__ == "__main__":
     app.run()
