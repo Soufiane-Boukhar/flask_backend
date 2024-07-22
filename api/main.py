c76b6b94eea7ff34b2c9003fbbb5263d4f6da64b
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-07-22 14:30:29 +0100

    add delete

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, Path
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import aiomysql
